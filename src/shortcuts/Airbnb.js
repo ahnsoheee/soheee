1a1ef7da387af0d25bd422556bfff6280cc473c4
ahnsoheee <61968474+ahnsoheee@users.noreply.github.com>
2021-01-01 13:41:27 +0900

    feat: airbnb

---
+++ src/shortcuts/Airbnb.js
@@ -0,0 +1,58 @@
+import ButtonBar from "../components/ButtonBar";
+import Image from "../components/Image";
+import Window from "../components/Window";
+import WindowContent from "../components/WindowContent";
+import AirbnbMain from "../image/airbnb.png";
+import AirbnbSearch from "../image/airbnbSearch.png";
+
+const Airbnb = ({ onClose }) => {
+  return (
+    <Window>
+      <ButtonBar text="Airbnb 클론.pdf" onClose={onClose} />
+      <WindowContent>
+        <br />
+        1. 소개
+        <br />
+        <br />
+        2020. 08. 31 ~ 2020. 09. 11
+        <br />
+        <br />
+        <br /> Airbnb 웹 서비스 클론
+        <br />
+        <br /> - 회원가입 및 로그인 기능
+        <br /> - 메인 페이지 UI
+        <br /> - 숙소 검색 기능
+        <br />
+        <br />
+        <br />
+        2. 기술 스택
+        <br />
+        <br />
+        - Front : JavaScript, HTML, CSS
+        <br />
+        - Back : Node.js, Express, NeDB
+        <br />
+        <br />
+        <br />
+        3. 메인 페이지
+        <br />
+        <br />
+        <Image src={AirbnbMain} width="500px" height="1000px" />
+        <br />
+        <br />
+        <br />
+        4. 숙소 검색 기능
+        <br />
+        <br />
+        - 지역, 체크인 및 체크아웃 날짜, 인원 입력 내용을 기반으로 예약 내역 확인 후 조회
+        <br />
+        <br />
+        <Image src={AirbnbSearch} width="500px" height="1000px" />
+        <br />
+        <br />
+      </WindowContent>
+    </Window>
+  );
+};
+
+export default Airbnb;
