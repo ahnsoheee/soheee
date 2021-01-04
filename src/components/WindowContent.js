00a7bbea85ccf4c1aa81d46e9b6d94138dc3699b
ahnsoheee <61968474+ahnsoheee@users.noreply.github.com>
2021-01-04 22:45:37 +0900

    beauty: width, border 속성 수정

--- src/components/WindowContent.js
+++ src/components/WindowContent.js
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 const WindowContent = styled.div`
-  width: 738px;
+  width: 750px;
   height: 500px;
   margin-left: 30px;
   overflow: auto;
@@ -11,9 +11,39 @@ const WindowContent = styled.div`
   }
   &::-webkit-scrollbar-thumb {
     background-color: #cccccc;
+    border-top: 2px solid #ffffff;
+    border-left: 2px solid #ffffff;
+    border-bottom: 2px solid #000000;
+    border-right: 2px solid #000000;
   }
   &::-webkit-scrollbar-track {
     background-color: #eeeeee;
+  }
+  ::-webkit-scrollbar-button:start:decrement {
+    display: block;
+    height: 16px;
+    border-top: 2px solid #ffffff;
+    border-left: 2px solid #ffffff;
+    border-bottom: 2px solid #000000;
+    border-right: 2px solid #000000;
+    background-color: #cccccc;
+    background-image: url(https://img.icons8.com/ios-filled/344/sort-up.png);
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: 10px 10px;
+  }
+  ::-webkit-scrollbar-button:end:increment {
+    display: block;
+    height: 16px;
+    border-top: 2px solid #ffffff;
+    border-left: 2px solid #ffffff;
+    border-bottom: 2px solid #000000;
+    border-right: 2px solid #000000;
+    background-color: #cccccc;
+    background-image: url(https://img.icons8.com/material-sharp/2x/give-way.png);
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: 9px 6px;
   }
 `;
 
