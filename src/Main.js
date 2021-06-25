990284e07c6d0e38a1a6a7b6d48b270747d28dac
ahnsoheee <soheeeahn@gmail.com>
2021-06-25 16:04:31 +0900

    가계부 프로젝트 추가

--- src/Main.js
+++ src/Main.js
@@ -1,13 +1,15 @@
 import React, { useState } from "react";
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 import Typing from "react-typing-animation";
 import Taxi from "./image/taxi.png";
 import GitHubBlack from "./image/github2.png";
 import AI from "./image/ai.png";
 import AirbnbLogo from "./image/airbnbLogo.png";
 import Laptop from "./image/laptop.png";
+import AccountbookLogo from "./image/accountbookLogo.png";
 import Blog from "./image/blog.png";
 import Mail from "./image/mail.png";
+import Notion from "./image/notion.png";
 import Icon from "./components/Icon";
 import FlexBox from "./components/FlexBox";
 import Uber from "./shortcuts/Uber";
@@ -15,6 +17,7 @@ import GitHub from "./shortcuts/GitHub";
 import Stock from "./shortcuts/Stock";
 import Airbnb from "./shortcuts/Airbnb";
 import Intern from "./shortcuts/Intern";
+import Accountbook from "./shortcuts/Accountbook";
 
 const Main = () => {
   const [skip, setSkip] = useState(false);
@@ -23,6 +26,7 @@ const Main = () => {
   const [stock, setStock] = useState(false);
   const [airbnb, setAirbnb] = useState(false);
   const [intern, setIntern] = useState(false);
+  const [accountbook, setAccountbook] = useState(false);
 
   return (
     <FlexBox onClick={() => setSkip(true)}>
@@ -53,59 +57,73 @@ const Main = () => {
             <br />
             어드바이저
           </Name>
+        </Shortcuts><br />
+        <br />
+        <Shortcuts>
+          <Icon src={AccountbookLogo} width="30px" height="30px" onClick={() => setAccountbook(true)} />
+          <Name>가계부</Name>
         </Shortcuts>
         <br />
         <br />
         <Shortcuts>
           <Icon src={Laptop} width="30px" height="30px" onClick={() => setIntern(true)} />
-          <Name>인턴 활동</Name>
+          <Name>인턴</Name>
         </Shortcuts>
       </Section>
       <Section margin="30px 0">
         {skip ? (
           <>
-            안소희 포트폴리오
-            <br />
-            <br />
-            <br />
-            경력 사항
-            <br />
-            <br />
-            2020.01 ~ 2020.02 태그하이브 개발팀 인턴
-            <br />
-            <br />
-            <br />
-            교육 이수
-            <br />
-            <br />
-            2020.07 ~ 2020.12 부스트 캠프
-            <br />
-            <br />
-            <br />
-            프로젝트
-            <br />
-            <br />
-            1. Uber 서비스 클론 – 택시 자버
-            <br />
-            2. GitHub 이슈 관리 서비스 클론
-            <br />
-            3. 가계부 서비스
-            <br />
-            4. Todo List
-            <br />
-            5. Airbnb 클론
-            <br />
-            6. 맞춤형 주식 추천 인공지능 – 로보 어드바이저
-            <br />
-            7. 사람 행동 판별 머신러닝
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
+            <Content>
+              안소희    
+              <br />
+              <br />
+              Full Stack Developer
+              <br />
+              <br />
+              <br />
+              * 기술
+              <br />
+              <br />
+              JavaScript TypeScript Python Java Node.js SpringBoot React Express Redux MySQL MongoDB Mongoose GraphQL 
+              <br />
+              <br />
+              <br />
+              * 경력
+              <br />
+              <br />
+              2020.01 ~ 2020.02 태그하이브 개발팀 인턴
+              <br />
+              <br />
+              <br />
+              * 자격증
+              <br />
+              <br />
+              1. 정보처리기사
+              <br />
+              2. 네트워크관리사 2급
+              <br />
+              3. SQL Developer
+              <br />
+              <br />
+              <br />
+              * 프로젝트
+              <br />
+              <br />
+              1. Uber 서비스 클론 – 택시 자버
+              <br />
+              2. GitHub 이슈 관리 서비스 클론
+              <br />
+              3. 가계부 서비스
+              <br />
+              4. Todo List
+              <br />
+              5. Airbnb 클론
+              <br />
+              6. 맞춤형 주식 추천 인공지능 – 로보 어드바이저
+              <br />
+              <br />
+              <br />
+            </Content>
             <FlexBox>
               <FlexBox style={{ cursor: "pointer" }} onClick={() => window.open("https://github.com/ahnsoheee/", "_black")}>
                 <Icon src={GitHubBlack} width="20px" height="20px" />
@@ -118,51 +136,75 @@ const Main = () => {
               </FlexBox>
               &nbsp; &nbsp;
               <FlexBox style={{ cursor: "pointer" }} onClick={() => window.open("mailto:soheeeahn@gmail.com")}>
+                <Icon src={Notion} width="20px" height="20px" />
+                Notion
+              </FlexBox>
+              &nbsp; &nbsp;
+              <FlexBox style={{ cursor: "pointer" }} onClick={() => window.open("mailto:soheeeahn@gmail.com")}>
                 <Icon src={Mail} width="20px" height="20px" />
                 Email: soheeeahn@gmail.com
               </FlexBox>
             </FlexBox>
           </>
         ) : (
-          <Typing speed={30}>
-            안소희 포트폴리오
-            <br />
-            <br />
-            <br />
-            경력 사항
-            <br />
-            <br />
-            2020.01 ~ 2020.02 태그하이브 개발팀 인턴
-            <br />
-            <br />
-            <br />
-            교육 이수
-            <br />
-            <br />
-            2020.07 ~ 2020.12 부스트 캠프
-            <br />
-            <br />
-            <br />
-            프로젝트
-            <br />
-            <br />
-            1. Uber 서비스 클론 – 택시 자버
-            <br />
-            2. GitHub 이슈 관리 서비스 클론
-            <br />
-            3. 가계부 서비스
-            <br />
-            4. Todo List
-            <br />
-            5. Airbnb 클론
-            <br />
-            6. 맞춤형 주식 추천 인공지능 – 로보 어드바이저
-            <br />
-            7. 사람 행동 판별 머신러닝
-            <br />
-            <br />
-            <Icon src={GitHubBlack} />
-          </Typing>
+          <>
+            <SkipWrapper>
+              <Blink>클릭하면 Skip !</Blink>
+            </SkipWrapper>
+            <Typing speed={30}>
+              <Content>
+                안소희
+                <br />
+                <br />
+                Full Stack Developer
+                <br />
+                <br />
+                <br />
+                * 기술
+                <br />
+                <br />
+                JavaScript TypeScript Python Java Node.js SpringBoot React Express Redux MySQL MongoDB Mongoose GraphQL 
+                <br />
+                <br />
+                <br />
+                * 경력
+                <br />
+                <br />
+                2020.01 ~ 2020.02 태그하이브 개발팀 인턴
+                <br />
+                <br />
+                <br />
+                * 자격증
+                <br />
+                <br />
+                1. 정보처리기사
+                <br />
+                2. 네트워크관리사 2급
+                <br />
+                3. SQL Developer
+                <br />
+                <br />
+                <br />
+                * 프로젝트
+                <br />
+                <br />
+                1. Uber 서비스 클론 – 택시 자버
+                <br />
+                2. GitHub 이슈 관리 서비스 클론
+                <br />
+                3. 가계부 서비스
+                <br />
+                4. Todo List
+                <br />
+                5. Airbnb 클론
+                <br />
+                6. 맞춤형 주식 추천 인공지능 – 로보 어드바이저
+                <br />
+                <br />
+                <br />
+              </Content>
+            </Typing>
+          </>
         )}
       </Section>
       {uber && <Uber onClose={() => setUber(false)} />}
@@ -170,9 +212,14 @@ const Main = () => {
       {stock && <Stock onClose={() => setStock(false)} />}
       {airbnb && <Airbnb onClose={() => setAirbnb(false)} />}
       {intern && <Intern onClose={() => setIntern(false)} />}
+      {accountbook && <Accountbook onClose={() => setAccountbook(false)} />}
     </FlexBox>
   );
 };
+
+const Content = styled.div`
+  padding-left: 15px;
+`;
 
 const Section = styled.div`
   width: fit-content;
@@ -189,6 +236,25 @@ const Shortcuts = styled.div`
 
 const Name = styled.div`
   width: 80px;
+`;
+
+const blinkingEffect = () => {
+  return keyframes`
+    50% {
+      opacity: 0;
+    }
+  `;
+}
+
+const Blink = styled.div`
+  animation: ${blinkingEffect} 1s linear infinite;
+`;
+
+const SkipWrapper = styled.div`
+  width: 200px; 
+  position: fixed;
+  bottom: 57px;
+  left: 20px
 `;
 
 export default Main;
